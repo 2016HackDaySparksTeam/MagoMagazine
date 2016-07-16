55714d05c2dad2c5e9436af4a7dbc1e3a757d2c7
渡辺雄貴 <watanabeyuuki@watanabeyuuki-no-macbook-air.local>
2016-07-16 15:37:15 +0900

    カメラ機能　実装完了

--- MagoMagazine/ViewController.swift
+++ MagoMagazine/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    // セッション.
+    var mySession : AVCaptureSession!
+    // デバイス.
+    var myDevice : AVCaptureDevice!
+    // 画像のアウトプット.
+    var myImageOutput : AVCaptureStillImageOutput!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // セッションの作成.
+        mySession = AVCaptureSession()
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+        
+        // バックカメラをmyDeviceに格納.
+        for device in devices{
+            if(device.position == AVCaptureDevicePosition.Back){
+                myDevice = device as! AVCaptureDevice
+            }
+        }
+        
+        // バックカメラからVideoInputを取得.
+        let videoInput: AVCaptureInput!
+        do {
+            videoInput = try AVCaptureDeviceInput.init(device: myDevice!)
+        }catch{
+            videoInput = nil
+        }
+        
+        // セッションに追加.
+        mySession.addInput(videoInput)
+        // 出力先を生成.
+        myImageOutput = AVCaptureStillImageOutput()
+        // セッションに追加.
+        mySession.addOutput(myImageOutput)
+        
+        // 画像を表示するレイヤーを生成.
+        let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.init(session:mySession)
+        myVideoLayer.frame = self.view.bounds
+        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        // Viewに追加.
+        self.view.layer.addSublayer(myVideoLayer)
+        
+        // セッション開始.
+        mySession.startRunning()
+        
+        // UIボタンを作成.
+        let myButton = UIButton(frame: CGRectMake(0,0,120,50))
+        myButton.backgroundColor = UIColor.redColor();
+        myButton.layer.masksToBounds = true
+        myButton.setTitle("撮影", forState: .Normal)
+        myButton.layer.cornerRadius = 20.0
+        myButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
+        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        
+        // UIボタンをViewに追加.
+        self.view.addSubview(myButton);
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // ボタンイベント.
+    func onClickMyButton(sender: UIButton){
+        // ビデオ出力に接続.
+        let myVideoConnection = myImageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        
+        // 接続から画像を取得.
+        self.myImageOutput.captureStillImageAsynchronouslyFromConnection(myVideoConnection, completionHandler: { (imageDataBuffer, error) -> Void in
+            
+            // 取得したImageのDataBufferをJpegに変換.
+            let myImageData : NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+            
+            // JpegからUIIMageを作成.
+            let myImage : UIImage = UIImage(data: myImageData)!
+            
+            // アルバムに追加.
+            UIImageWriteToSavedPhotosAlbum(myImage, self, nil, nil)
+        })
     }
-
-
 }
-
